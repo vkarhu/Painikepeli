df2f9d15da5656913580620ce54ac0c3f739510e
Ville Karhu <vkarhu8@gmail.comgit config --global user.name Ville>
2020-02-16 16:03:18 +0200

    cow test

--- painikepeli-frontend/src/App.js
+++ painikepeli-frontend/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { Component } from 'react'
+import './App.css'
+class App extends Component {
+  state = {
+    cow: '',
+    text: ''
+  }
+  componentDidMount() {
+    this.fetchCow()
+  }fetchCow = async () => {
+    const response = await fetch(`/api/cow`)
+    const initialCow = await response.json()
+    const cow = initialCow.moo
+    this.setState({ cow })
+  }
+  customCow = async evt => {
+    evt.preventDefault()
+    const text = this.state.text
+    const response = await fetch(`/api/cow/${text}`)
+    const custom = await response.json()
+    const cow = custom.moo
+    this.setState({ cow, text: '' })
+  }
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value })
+    console.log(this.state.text)
+  }
+  render() {
+    return (
+      <div className="App">
+        <h3>Text Cow. Moo</h3>
+        <code>{this.state.cow}</code>
+        <form onSubmit={this.customCow}>
+          <label>Custom Cow Text:</label>
+          <input
+            type="text"
+            name="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Show me a talking cow!</button>
+        </form>
+      </div>
+    )
+  }
+}export default App
